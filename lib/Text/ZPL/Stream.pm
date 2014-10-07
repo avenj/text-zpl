44f4e3f73a2973864e80a17beffa7003257eb4aa
Jon Portnoy <avenj@cobaltirc.org>
2014-10-07 09:22:12 -0400

    POD, drop 'string' constructor param
    Still a bit undecided on this one, but calling ->push yourself after
    constructing a new stream is not much of a burden.

--- lib/Text/ZPL/Stream.pm
+++ lib/Text/ZPL/Stream.pm
@@ -19,7 +19,7 @@ sub new {
   #  string =>
   my ($class, %param) = @_;
   my $root = +{};
-  my $self = bless [
+  bless [
     ($param{max_buffer_size} || 0),  # BUF_MAX
     '',                              # BUF
     0,                               # MAYBE_EXTRA_EOL
@@ -27,11 +27,7 @@ sub new {
     $root,                           # CURRENT
     0,                               # LEVEL
     [],                              # TREE
-  ], $class;
-
-  $self->push($param{string}) if defined $param{string};
-
-  $self
+  ], $class
 }
 
 
@@ -148,3 +144,91 @@ sub push {
 
 
 1;
+
+=pod
+
+=head1 NAME
+
+Text::ZPL::Stream - Streaming ZPL decoder
+
+=head1 SYNOPSIS
+
+  use Text::ZPL::Stream;
+
+  my $stream = Text::ZPL::Stream->new;
+
+  while ( defined(my $chrs = magically_get_some_zpl) ) {
+    $stream->push($chrs);
+  }
+
+  my $ref = $stream->get;
+
+=head1 DESCRIPTION
+
+A streaming decoder for C<ZeroMQ Property Language> files using L<Text::ZPL>. 
+
+See the L<Text::ZPL> documentation for more on C<ZPL> and parsing-related
+details.
+
+=head2 new
+
+  my $stream = Text::ZPL::Stream->new(
+    max_buffer_size => 512,
+  );
+
+Constructs an object representing a new C<ZPL> stream.
+
+Accepts the following options:
+
+=over
+
+=item max_buffer_size
+
+The maximum line length allowed in buffers before an exception is thrown.
+
+Defaults to 0 (unlimited).
+
+=back
+
+=head2 push
+
+  $stream->push(@lines);
+  $stream->push(@chars);
+  $stream->push($string);
+
+Takes characters (individually or as strings) and collects until an
+end-of-line marker is encountered, at which point a parse is called and the
+reference returned by L</get> is altered appropriately.
+
+An exception is thrown if parsing fails, or if L</max_buffer_size> is reached
+-- if you're unsure of your incoming data, you may want to wrap C<push> calls
+with L<Try::Tiny> or similar.
+
+Returns the number of complete lines parsed, which can be useful as an
+indicator that L</get> ought be called:
+
+  if ( $stream->push($zpl) ) {
+    # Parsed at least one complete line:
+    my $ref = $stream->get;
+    ...
+  }
+
+=head2 get
+
+  my $ref = $stream->get;
+
+B<< This is the actual reference in use by the decoder, not a copy! >>
+Altering the structure of the C<HASH> may have unintended consequences, in
+which case you may want to make use of L<Storable/dclone> to create a safe
+copy.
+
+=head2 get_buffer
+
+Returns a string containing the current character buffer (that is, any
+currently incomplete line).
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+=cut
